7b6d6e332be60da94ec10f78c97ea384e2d23d73
m0byfloyd <alexandre.robert2810@gmail.com>
2022-05-05 15:02:34 +0200

    Kevin c'est pour toi

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,44 +3,93 @@ import * as matchers from "jest-extended";
 import {caculateConvertedExpression, calculateRNP} from "./index";
 expect.extend(matchers);
 
-describe('Test caculateConvertedExpression()',()=> {
-  it('Calculate basic expression', () => {
-    //GIVEN
+describe('Batterie de test', function () {
+  describe('Test caculateConvertedExpression()',()=> {
+    it('Calculate basic expression', () => {
+      //GIVEN
 
-    //WHEN
-    const actual = caculateConvertedExpression(7 , 3, '*');
+      //WHEN
+      const actual = caculateConvertedExpression(7 , 3, '*');
 
-    //THEN
-    const expected = 21;
-    expect(actual).toEqual(expected)
+      //THEN
+      const expected = 21;
+      expect(actual).toEqual(expected)
+    });
   });
-});
 
-describe('Test calculateRNP()',()=> {
-  it('test if is not a string',()=> {
-    //GIVEN
-    const expression = [1, 2, '-'];
+  describe('Test calculateRNP()',()=> {
+    it('Test addition',()=> {
+      //GIVEN
+      const expression = [10, 2, '+'];
 
-    //WHEN
-    const actual = calculateRNP(expression);
+      //WHEN
+      const actual = calculateRNP(expression);
 
-    //THEN
-    const expected = -1;
-    expect(actual).toEqual(expected)
+      //THEN
+      const expected = 12;
+      expect(actual).toEqual(expected)
+    });
+
+    it('Test minus',()=> {
+      //GIVEN
+      const expression = [14, 8, '+'];
+
+      //WHEN
+      const actual = calculateRNP(expression);
+
+      //THEN
+      const expected = 22;
+      expect(actual).toEqual(expected)
+    });
+
+    it('Test minus',()=> {
+      //GIVEN
+      const expression = [1, 2, '-'];
+
+      //WHEN
+      const actual = calculateRNP(expression);
+
+      //THEN
+      const expected = -2;
+      expect(actual).toEqual(expected)
+    });
+
+    it('Test multiplication',()=> {
+      //GIVEN
+      const expression = [10, 2, '*'];
+
+      //WHEN
+      const actual = calculateRNP(expression);
+
+      //THEN
+      const expected = 20;
+      expect(actual).toEqual(expected)
+    });
+
+    it('Test addition and multiplication',()=> {
+      //GIVEN
+      const expression = [1, 2, '+', 4, '*'];
+
+      //WHEN
+      const actual = calculateRNP(expression);
+
+      //THEN
+      const expected = 12;
+      expect(actual).toEqual(expected)
+    });
+
+    it('Test multiplication and addition',()=> {
+      //GIVEN
+      const expression = [1, 2, '*', 4, '+'];
+
+      //WHEN
+      const actual = calculateRNP(expression);
+
+      //THEN
+      const expected = 6;
+      expect(actual).toEqual(expected)
+    });
   });
-});
 
-describe('Test calculateRNP() 2eme',()=> {
-  it('test if is not a string',()=> {
-    //GIVEN
-    const expression = [1, 2, '+', 4, '*'];
-
-    //WHEN
-    const actual = calculateRNP(expression);
-
-    //THEN
-    const expected = -3;
-    expect(actual).toEqual(expected)
-  });
-});
+})
 
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,61 @@
 export function calculateRNP(expression: Array<any>) {
-    let cal: Array<any> = [];
+    let operandList: Array<any> = [];
     const result: Array<any> = [];
+
+    //Pour chaque élément de l'expression
     expression.forEach(function (value: number | string) {
+        console.log(typeof  value)
+
+        // Si l'élément est un nombre
         if (typeof value === 'number') {
-            if (cal.length > 1) {
-                result.push(cal[0]);
-                cal[0] = cal[1];
-                cal[1] = value;
-            } else {
-                cal.push(value);
+            //Si il ne manque pas d'élément pour un calcul (deux opérandes)
+            if (operandList.length > 1) {
+                result.push(operandList[0]);
+                operandList[0] = operandList[1];
+                operandList[1] = value;
+
+            }
+            // Si c'est le premier on push direct dans le tableau de calcul
+            else {
+                operandList.push(value);
             }
         } else {
-            if (cal.length === 2) {
-                result.push(caculateConvertedExpression(cal[0], cal[1], value));
-                cal = [];
+            // Si le calcul a deux opérandes
+            if (operandList.length === 2) {
+                //On calcule avec ces deux opérandes et la valeur (qui est ici l'opérateur)
+                result.push(caculateConvertedExpression(operandList[0], operandList[1], value));
+                operandList = [];
             } else {
                 result.push(value)
-                cal = [];
+                operandList = [];
             }
         }
     });
-    if (cal.length > 0) {
-        result.concat(cal);
+
+    if (operandList.length > 0) {
+        result.concat(operandList);
     }
     if (result.length > 1) {
         result[0] = calculateRNP(result);
     }
+
     return result[0];
 }
 
-export function caculateConvertedExpression(firstN: number, secondN: number, operator: string): number {
-    let result = firstN + secondN;
+export function caculateConvertedExpression(firstOperand: number, secondOperand: number, operator: string): number {
+    let result = firstOperand + secondOperand;
+
     switch (operator) {
         case '-': {
-            result = firstN - secondN;
+            result = firstOperand - secondOperand;
             break;
         }
         case '*': {
-            result = firstN * secondN;
+            result = firstOperand * secondOperand;
             break;
         }
         case '/': {
-            result = firstN / secondN;
+            result = firstOperand / secondOperand;
         }
     }
     return result;
